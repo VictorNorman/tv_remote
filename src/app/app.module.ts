f0c8ab72f4c1f90843f5909bc1f2ca2619124e12
Victor Norman <vtn2@calvin.edu>
2021-08-27 11:00:26 -0400

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SSHConnect } from '@ionic-native/ssh-connect/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SSHConnect,
+    Media,
+    File,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SSHConnect } from '@ionic-native/ssh-connect/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,77 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public buttonMsg = 'Record';
+  private audioFile: MediaObject = null;
 
+  constructor(private sshConnect: SSHConnect, private media: Media, private file: File) {
+    this.audioFile = this.media.create(this.file.externalRootDirectory + '/audioFile.mp3');
+  }
+
+  async toggleOnOff() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_POWER');
+  }
+
+  closedCaptioningOn() {
+    this.sendCommand('sh cc_on.sh');
+  }
+  closedCaptioningOff() {
+    this.sendCommand('sh cc_off.sh');
+  }
+  toggleMute() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_MUTE');
+  }
+
+  volumeUp() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_VOLUMEUP');
+  }
+
+  volumeDown() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_VOLUMEDOWN');
+  }
+
+  channelUp() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_CHANNELUP');
+  }
+  channelDown() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_CHANNELDOWN');
+  }
+
+  goToCh8() {
+    this.sendCommand('irsend send_once JVC_RMT-JC02 KEY_8');
+  }
+  goToCW() {
+    this.sendCommand('sh cw.sh');
+  }
+  goToDabl() {
+    this.sendCommand('sh dabl.sh');
+  }
+  goToQuest() {
+    this.sendCommand('sh quest.sh');
+  }
+
+  toggleRecordingAudio() {
+    console.log('recording...');
+
+    if (this.buttonMsg === 'Record') {
+      this.audioFile.startRecord();
+       this.buttonMsg = 'Stop';
+    } else {
+      this.audioFile.stopRecord();
+      this.buttonMsg = 'Record';
+    }
+  }
+
+  private async sendCommand(cmd: string) {
+    try {
+      let resp = await this.sshConnect.connect('pi', '1', '192.168.2.219', 22);
+      console.log(resp);
+      resp = await this.sshConnect.executeCommand(cmd);
+      console.log(resp);
+      resp = this.sshConnect.disconnect();
+      console.log(resp);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
